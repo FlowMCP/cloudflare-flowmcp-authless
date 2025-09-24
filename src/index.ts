745886386be9cdeac87729bfb153c33fdd8d9010
αηdr3α5 ɮαηɦօlʐ3ʀ <AndreasBanholzer@Me.com>
2025-09-24 02:19:31 +0200

    insert flowmcp

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,13 @@ import { FlowMCP } from "flowmcp";
 import { SchemaImporter } from "schemaimporter";
 import { schema as pingSchema } from "../custom-schemas/ping.mjs";
 
+const arrayOfSchemas = await SchemaImporter.loadFromFolder({
+	excludeSchemasWithImports: true,
+	excludeSchemasWithRequiredServerParams: true,
+	addAdditionalMetaData: false,
+});
+
+
 // Define our MCP agent with tools
 export class MyMCP extends McpAgent {
 	server = new McpServer({
